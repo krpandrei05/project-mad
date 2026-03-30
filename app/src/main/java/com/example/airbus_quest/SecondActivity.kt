bff5781f9555c9657f3724d2e1c9a137db17f813
Andrei Carp <andrei.krp@gmail.com>
2026-03-30 12:50:18 +0300

    feat(second-activity): add User Identifier dialog and SharedPreferences read/write
    Signed-off-by: Andrei Carp <andrei.krp@gmail.com>

--- app/src/main/java/com/example/airbus_quest/SecondActivity.kt
+++ app/src/main/java/com/example/airbus_quest/SecondActivity.kt
@@ -4,7 +4,10 @@ import android.content.Intent
 import android.os.Bundle
 import android.util.Log
 import android.widget.Button
+import android.widget.EditText
+import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AlertDialog
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
@@ -38,5 +41,61 @@ class SecondActivity : AppCompatActivity() {
             val intent = Intent(this, ThirdActivity::class.java)
             startActivity(intent)
         }
+
+        val btnuserIdentifier: Button = findViewById(R.id.btnUserIdentifier)
+        btnuserIdentifier.setOnClickListener {
+            Log.d(SecondTag, "Click: Opening User ID dialog.")
+            showUserIdentifierDialog()
+        }
+
+        val userIdentifier = getUserIdentifier()
+        if (userIdentifier == null) {
+            showUserIdentifierDialog()
+        } else {
+            Toast.makeText(this, "User ID: $userIdentifier", Toast.LENGTH_LONG).show()
+        }
+    }
+
+    private fun showUserIdentifierDialog() {
+        val builder = AlertDialog.Builder(this)
+        builder.setTitle("Enter User Identifier")
+
+        val input = EditText(this)
+        val existing = getUserIdentifier()
+        if (existing != null) {
+            input.setText(existing)
+        }
+        builder.setView(input)
+
+        builder.setPositiveButton("OK") { _, _ ->
+            val userInput = input.text.toString()
+            if (userInput.isNotBlank()) {
+                saveUserIdentifier(userInput)
+                Toast.makeText(this, "User ID saved: $userInput", Toast.LENGTH_LONG).show()
+            } else {
+                Toast.makeText(this, "User ID cannot be blank", Toast.LENGTH_LONG).show()
+            }
+        }
+
+        builder.setNegativeButton("Cancel") { dialog, _ ->
+            Toast.makeText(this, "Thanks and goodbye!", Toast.LENGTH_LONG).show()
+            dialog.cancel()
+        }
+
+        builder.show()
+    }
+
+    private fun saveUserIdentifier(userIdentifier: String) {
+        val sharedPreferences = this.getSharedPreferences("AppPreferences", MODE_PRIVATE)
+        sharedPreferences.edit().apply {
+            putString("userIdentifier", userIdentifier)
+            apply()
+        }
+    }
+
+
+    private fun getUserIdentifier(): String? {
+        val sharedPreferences = this.getSharedPreferences("AppPreferences", MODE_PRIVATE)
+        return sharedPreferences.getString("userIdentifier", null)
     }
 }
